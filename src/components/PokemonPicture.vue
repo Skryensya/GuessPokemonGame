4e508587a34412de41381d390dde5556d14c8c7f
Allison Peña <skryensya@gmail.com>
2022-01-18 03:33:03 -0300

    Se agrega el proyecto

---
+++ src/components/PokemonPicture.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="pokemon-container">
+
+  <img v-if="!showPokemon" :src="imgSrc"
+  class="hidden-pokemon"
+  alt="pokemon">
+
+  <img v-else :src="this.imgSrc"
+  class="fade-in"
+  alt="pokemon">
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    pokemonId: {
+      type: Number,
+      default: 1,
+      required: true, 
+      validator( value ) {
+        return value > 0 && value < 600
+      }
+    },
+    showPokemon:{
+      type: Boolean,
+      required: true,
+      default: false
+    }
+  },
+  computed:{
+    imgSrc(){
+      
+      return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${this.pokemonId}.svg`
+      
+    }
+    
+  }
+}
+</script>
+
+<style scoped>
+.pokemon-container {
+    height: 200px;
+}
+img {
+    height: 200px;
+    user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    -webkit-user-drag: none;
+    -webkit-user-select: none;
+}
+.hidden-pokemon {
+    filter: brightness(0);
+}
+
+</style>
